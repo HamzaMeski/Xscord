cd5e5a656e2331bfdf94d19f1df8107456121fcb
Hamza Meski <meskihamza5@gmail.com>
2025-01-13 10:57:15 +0100

    SERVER, UI setup

---
+++ SERVER/src/main/java/com/discord/SERVER/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.discord.SERVER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ SERVER/src/test/java/com/discord/SERVER/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discord.SERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
