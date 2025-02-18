edae24197a6c5cbff0ca7002f419ac8acf7dac49
HamzaMeski <meskihamza5@gmail.com>
2025-02-18 09:05:03 +0100

    fixing auth issues - SERVER

--- SERVER/src/main/java/com/discord/SERVER/entities/Admin.java
+++ SERVER/src/main/java/com/discord/SERVER/entities/Admin.java
@@ -2,15 +2,16 @@ package com.discord.SERVER.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.persistence.TableGenerator;
-import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
+@Setter
+@Getter
 @Table(name = "admins")
 public class Admin extends User {
 }
