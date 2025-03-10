20d733f0020d52639be5695694ab239a2540e9a2
HamzaMeski <meskihamza5@gmail.com>
2025-03-10 15:29:07 +0000

    server mng state manager

--- UI/src/app/ngrx/effects/server/server.effects.ts
+++ UI/src/app/ngrx/effects/server/server.effects.ts
@@ -1,17 +1,34 @@
 import {Injectable} from "@angular/core";
-import {Actions} from "@ngrx/effects";
-import {Store} from "@ngrx/store";
+import {Actions, createEffect, ofType} from "@ngrx/effects";
 import {ServerService} from "../../../core/services/restfull/backend/server.service";
+import {createServer, createServerFailure, createServerSuccess} from "../../actions/server/server.actions";
+import {catchError, map, mergeMap, of} from "rxjs";
 
 
 @Injectable()
 export class ServerEffects {
+	createServer$
+
 
 	constructor(
 		private actions$: Actions,
-		private store: Store,
 		private serverService: ServerService
 	) {
-
+		this.createServer$ = createEffect(() =>
+			this.actions$.pipe(
+				ofType(createServer),
+				mergeMap(({request}) =>
+					this.serverService.createServer(request).pipe(
+						map(response => {
+							return createServerSuccess({response});
+						}),
+						catchError(err => {
+							const error: string = err.error.message
+							return of(createServerFailure({error}));
+						})
+					)
+				)
+			)
+		)
 	}
 }
