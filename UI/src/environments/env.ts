fa158198120af465883e0a9e3ad3514024741942
HamzaMeski <meskihamza5@gmail.com>
2025-05-24 14:28:09 +0100

    env

--- UI/src/environments/env.ts
+++ UI/src/environments/env.ts
@@ -1,3 +1,3 @@
-
+// this is just for testing purposes, keys shouldn't be available in such us palce
 export const serverApiUrl: string = "http://localhost:8080/api/v1"
 export const openRouterApiKey: string = "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"
